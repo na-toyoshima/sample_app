0b085ee6611d8b6d95b8820d50daeaf60c7f3109
na-toyoshima <na.toyoshim@gmail.com>
2020-03-19 17:53:26 +0900

    [finish]4-9

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '1102f6043735deb2ada462bff45b77c8533a0dc1e84ddb6a072c7d674f6d1920bae3c5034f47383e2000f135b32c9b84381ad2aa7a5ab0ceb955cc739a35ba46'
